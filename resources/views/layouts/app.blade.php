ce68d4997293e73a0f2d114db9cce1b0ecffd47e
sawasawa0 <sawasawaworks@gmail.com>
2017-08-01 03:42:18 +0000

    rakuten api

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
         <link rel="stylesheet" href="css/style.css">
+        
     </head>
     <body>
         @include('commons.navbar')
